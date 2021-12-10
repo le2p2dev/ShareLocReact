099fbe9fa6f47f7e1223a6cf7e1e445dc24fac39
yassinmeghazi <BRN2702emya&*>
2021-12-10 11:08:31 +0100

    [ADD] basic login method, have to add fetch and css

--- sharelocreact/src/App.js
+++ sharelocreact/src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 import {Link} from "react-router-dom";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-        <nav
-            style={{
-                borderBottom: "solid 1px",
-                paddingBottom: "1rem"
-            }}
-        >
-      <h1>ShareLoc</h1>
-        <Link to="/signin"> ToSignin</Link>
-        <Link to="/signup"> ToSingup </Link>
-        </nav>
-    </div>
-  );
+
+
+    return (
+        <>
+
+            <nav>
+                <h1>ShareLoc</h1>
+                <Link to="/signin"> ToSignin</Link>
+                <Link to="/signup"> ToSingup </Link>
+            </nav>
+
+
+        </>
+    );
 }
 
 export default App;
--- sharelocreact/src/routes/Signin.js
+++ sharelocreact/src/routes/Signin.js
@@ -1,7 +1,42 @@
-export default function Signin() {
+import {useState} from "react";
+
+export default function Signin( ) {
+
+
+    const [user, setUser] = useState({login: "", password: ""})
+    const [error, setError ] = useState("")
+
+    const Login = (details) => {
+        console.log(details)
+    }
+
+    const Submit = (e) => {
+        e.preventDefault();
+        Login(user)
+    }
+
+
     return (
-        <main style={{ padding: "1rem 0" }}>
-            <h2>Signin</h2>
-        </main>
+       <>
+           <form onSubmit={Submit}>
+               <div className="form-inn">
+                   <h2>Login</h2>
+                   <div className="form-group">
+                       <label htmlFor="login">login</label>
+                       <input type="text" name="login" id="loginInpt"
+                              value={user.login} onChange={ e => setUser({...user, login: e.target.value}) }
+                       />
+                   </div>
+                   <div className="form-group">
+                       <label htmlFor="password">password</label>
+                       <input type="password" name="password" id="passwordInpt"
+                              value={user.password} onChange={ e => setUser({...user, password: e.target.value}) }
+                       />
+                   </div>
+                   <input type="submit" value="submit"/>
+               </div>
+           </form>
+
+       </>
     );
 }
